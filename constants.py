63426cf0a8895a899b889605e2ea59ba4a51c3c4
Manav010203 <manav01022003@gmail.com>
2025-09-05 21:13:57 +0530

    const

---
+++ constants.py
@@ -0,0 +1,9 @@
+import pygame
+WIDHT = 900
+HEIGHT = 600
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+GREEN = (0,255,0)
+
+screen = pygame.display.set_mode((1280,720))
+# font20 = pygame.font.Font('freesansbold.ttf',20)
